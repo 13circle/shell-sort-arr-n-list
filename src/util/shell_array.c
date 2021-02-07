01d81112d7b1322eff32d6cee5f186ca63034bdf
13circle <13circle97@gmail.com>
2021-02-08 04:04:28 +0900

    Add comments & remove unused var

--- src/util/shell_array.c
+++ src/util/shell_array.c
@@ -10,31 +10,38 @@
 #include "../lib/shell_array.h"
 
 long *Array_Load_From_File(char *filename, int *size) {
-  struct stat st;      // Status structure of the file
-  FILE *fp;            // FILE pointer
-  int i;
-  long *arr, n;
+  struct stat st;  // Status structure of the file
+  FILE *fp;        // FILE pointer
+  int i;           // Loop index
+  long *arr;       // Input array
 
+  // Get file info using stat structure & handle exception
+  // (stat() returns nonzero value on error)
   if (stat(filename, &st)) {
     fprintf(stderr, "'%s': File does not exist \n", filename);
     return NULL;
   }
 
+  // Open file as read mode for a binary file & handle exception
   if (!(fp = fopen(filename, "rb"))) {
     fprintf(stderr, "'%s': Cannot read this file \n", filename);
     return NULL;
   }
 
+  // Calculate the size of the array
   *size = st.st_size / sizeof(long);
 
+  // Allocate memory for the array & handle exception
   arr = (long *)malloc(sizeof(long) * (*size));
   if (!arr) {
     fprintf(stderr, "Insufficent Memory: Cannot allocate memory \n");
     return NULL;
   }
 
+  // Read long-type integers from the file
   fread(arr, sizeof(long), *size, fp);
 
+  // Close the input file & return the array
   fclose(fp);
   return arr;
 }
